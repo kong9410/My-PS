f4233e93fca9c9b49167b9f49c1e8079193823c3
KONG <37204770+kong9410@users.noreply.github.com>
2019-06-30 23:23:19 +0900

    Create samsung_7829.cpp

---
+++ samsung_7829.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+long long *arr;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int test_case;
+    int j = 1;
+    cin >> test_case;
+    while(test_case--){
+        int p;
+        cin >> p;
+        arr = new long long[p];
+        for(int i = 0; i < p; i++){
+            cin >> arr[i];
+        }
+        if(p == 1){
+            cout << "#" << j++ << " " << arr[0] << endl;
+        }
+        else{
+            sort(arr, arr+p);
+            cout << "#" << j++ << " " << arr[0] * arr[p-1] << endl;
+        }
+    }
+
+    return 0;
+}
