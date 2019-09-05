932e9faad978f83fd864e891dad1bfb5f5b07e70
KONG <37204770+kong9410@users.noreply.github.com>
2019-09-05 21:31:40 +0900

    Update programmers_best_album.cpp

--- programmers_best_album.cpp
+++ programmers_best_album.cpp
@@ -10,11 +10,32 @@
 #include <iostream>
 using namespace std;
 
+struct tsum{
+    int sum;
+    int idx; 
+    string name;
+};
+
+// 고려할 필요없는데 만들어버렸음
+// 만든김에 안지움
+bool comp(tsum a, tsum b){
+    if(a.sum == b.sum){
+        return a.idx < b.idx;
+    }
+    return a.sum > b.sum;
+}
+
+bool idxcomp(pair<int, int> a, pair<int, int> b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }
+    return a.first > b.first;
+}
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     map<string, vector<pair<int, int>>> mp;
-    vector<pair<int, pair<int, string>>> t_sum;
+    vector<tsum> t_sum;
     for(int i = 0; i < genres.size(); i++){
         mp[genres[i]].push_back({plays[i], i});
     }
@@ -22,24 +43,24 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     // 장르 합이 제일 큰것을 찾기
     for(auto m = mp.begin(); m != mp.end(); m++){
         int sum = 0;
+        int minidx = 10001;
         for(int j = 0; j < genres.size(); j++){
             if(m->first == genres[j]){
                 sum += plays[j];
+                minidx = min(minidx, j);
             }
         }
-        t_sum.push_back({sum, m->first});
+        t_sum.push_back({sum, minidx, m->first});
     }
-    sort(t_sum.begin(), t_sum.end());
-    reverse(t_sum.begin(), t_sum.end());
+    sort(t_sum.begin(), t_sum.end(), comp);
     
     // 정렬된 키 순으로 2개씩 구하기
     for(auto m = mp.begin(); m != mp.end(); m++){
-        sort(m->second.begin(), m->second.end());
-        reverse(m->second.begin(), m->second.end());
+        sort(m->second.begin(), m->second.end(), idxcomp);
     }
     for(int i = 0; i < t_sum.size(); i++){
-        for(int j = 0; j < mp[t_sum[i].second].size() && j < 2; j++){
-            answer.push_back(mp[t_sum[i].second][j].second);
+        for(int j = 0; j < mp[t_sum[i].name].size() && j < 2; j++){
+            answer.push_back(mp[t_sum[i].name][j].second);
         }
     }
     return answer;
